36fb0eb9837d0e3d0f5024192919da7c98cfdec9
MWCRMPRO <osnenufu@gmail.com>
2026-07-10 06:33:11 +0300

    fix: rerun loop kırıldı - sayac artırılarak widget boş geliyor, hata yok

--- main.py
+++ main.py
@@ -3223,10 +3223,16 @@ function kartSec(id){
         ara_txt = _fc[1].text_input("a", placeholder="🔍 Firma, yetkili, il...", key="ara_liste", label_visibility="collapsed")
 
         _fk_sfx = st.session_state.get("_filtre_reset_sayac", 0)
-        _asama_def = [x for x in st.session_state.get("_cl_fil_asama_multi",[]) if x in tum_asama_opts]
-        _asama_sec = _fc[2].multiselect("a", tum_asama_opts, default=_asama_def, key=f"_cl_fil_asama_multi_{_fk_sfx}", placeholder="Aşama...", label_visibility="collapsed")
+        _asama_def = [] if st.session_state.get("_toplam_aktif") else [x for x in st.session_state.get("_cl_fil_asama_multi",[]) if x in tum_asama_opts]
+        _asama_sec_raw = _fc[2].multiselect("a", ["🔵 Tümü"] + tum_asama_opts, default=_asama_def, key=f"_cl_fil_asama_multi_{_fk_sfx}", placeholder="Aşama...", label_visibility="collapsed")
+        if "🔵 Tümü" in _asama_sec_raw:
+            st.session_state["_toplam_aktif"] = True
+            st.session_state["_filtre_reset_sayac"] = _fk_sfx + 1
+            for _fk in ["_cl_fil_durum_multi","_cl_fil_asama_multi","_cl_fil_il_multi","_cl_fil_ilce_multi","_cl_fil_temsilci_multi"]:
+                if _fk in st.session_state: del st.session_state[_fk]
+            st.rerun()
+        _asama_sec = _asama_sec_raw
         st.session_state["_cl_fil_asama_multi"] = _asama_sec
-
         _durum_opts_tumu = ["Tümü"] + [x for x in tum_durum_opts if str(x).upper() not in ["NONE","NAN",""]]
         _durum_def = [x for x in st.session_state.get("_cl_fil_durum_multi",[]) if x in _durum_opts_tumu]
         _durum_sec_raw = _fc[3].multiselect("d", _durum_opts_tumu, default=_durum_def, key=f"_cl_fil_durum_multi_{_fk_sfx}", placeholder="Durum...", label_visibility="collapsed")
